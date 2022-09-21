0da20668497bd837758350c4b86aacbd09083005
ElFranklin29 <franklin2908david@gmail.com>
2022-09-20 21:16:59 -0500

    update package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { TaskCreator } from './components/TaskCreator'
+import { TaskTable } from './components/TaskTable'
+import { VisibilityControl } from './components/VisibilityControl';
 
 function App() {
+
+  const [taskItems, setTasksItems] = useState([]);
+  const [showCompleted, setShowCompleted] = useState(false)
+
+
+
+  function createNewTask(taskName) {
+    if (!taskItems.find(task => task.name === taskName)) {
+      setTasksItems([...taskItems, { name: taskName, done: false }])
+    }
+  }
+
+  const toggleTask = task => {
+    setTasksItems(
+      taskItems.map(t => (t.name === task.name) ? { ...t, done: !t.done } : t)
+    )
+
+  }
+
+  useEffect(() => {
+    let data = localStorage.getItem('tasks')
+    if (data) {
+      setTasksItems(JSON.parse(data))
+    }
+  }, [])
+
+  const cleanTasks = () => {
+    setTasksItems(taskItems.filter(task => !task.done))
+    setShowCompleted(false)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(taskItems))
+  }, [taskItems])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-dark vh-100 text-white">
+      <div className='container p-4 col-md-4 offset-md-4'>
+        <TaskCreator createNewTask={createNewTask} />
+        <TaskTable tasks={taskItems} toggleTask={toggleTask} />
+        <VisibilityControl
+          isChecked={showCompleted}
+          setShowCompleted={(checked) => setShowCompleted(checked)}
+          cleanTasks={cleanTasks}
+        />
+        {
+          showCompleted === true && (
+            <TaskTable tasks={taskItems} toggleTask={toggleTask} showCompleted={showCompleted} />
+          )
+        }
+     </div>
+    </main>
   );
 }
 
